a3f72e079d603bd3cd7fb8496e4e89676d91388c
yohan211 <gamishini37@gmail.com>
2023-10-11 07:49:03 -0700

    pih

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,33 +1,6 @@
-#include <stdlib.h>
-#include "main.h"
+#ifndef PI_H
+#define PI_H
 
-/**
- * *_strdup - copies the string given as parameter
- * @str: string to duplicate
- *
- * Return: pointer to the copied string (Success), NULL (Error)
- */
-char *_strdup(char *str)
-{
-	char *dup;
-	unsigned int i, len;
+#define PI 3.14159265359
 
-	i = 0;
-	len = 0;
-
-	if (str == NULL)
-		return (NULL);
-
-	while (str[len])
-		len++;
-
-	dup = malloc(sizeof(char) * (len + 1));
-
-	if (dup == NULL)
-		return (NULL);
-
-	while ((dup[i] = str[i]) != '\0')
-		i++;
-
-	return (dup);
-}
+#endif
